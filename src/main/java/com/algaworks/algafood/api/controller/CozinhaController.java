c52d77975deaa7a79867912da6bc31ba442f6f7b
jadeilson.correia <jadeilson.correia@host.docker.internal>
2023-06-07 01:47:18 -0300

    Aula 5.1 - Implementado URI de filtro de cozinhas no controller de cozinhas. Utilizado JPQL para realizar o filtro de like na base.

--- src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -40,9 +41,9 @@ public class CozinhaController {
 	}
 	
 	@GetMapping
-	@RequestMapping(value = "/filtra-cozinha/{nomeCozinha}")
+	@RequestMapping(value = "/filtra-cozinha")
 	@ResponseStatus(value = HttpStatus.OK)
-	public List<Cozinha> filtraCozinha(@PathVariable String nomeCozinha){
+	public List<Cozinha> filtraCozinha(@RequestParam String nomeCozinha){
 		return cozinhaRepository.filtraCozinha(nomeCozinha);
 	}
 	
