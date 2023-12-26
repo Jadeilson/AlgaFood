da5f237aff038c16897ec5f9310c647fa4ac0f9d
Jadeilson Correia <jadeilson.correia@softplan.com.br>
2023-12-26 17:29:09 -0300

    Commit para migração de maquina

--- src/main/java/com/algaworks/algafood/domain/model/ItemPedido.java
+++ src/main/java/com/algaworks/algafood/domain/model/ItemPedido.java
@@ -1,5 +1,7 @@
 package com.algaworks.algafood.domain.model;
 
+import java.math.BigDecimal;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -14,18 +16,33 @@ import lombok.EqualsAndHashCode;
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
-public class Cidade {
+public class ItemPedido {
 	
 	@Id
 	@EqualsAndHashCode.Include
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-	@Column(nullable = false, name = "cidade_nome")
-	private String nome;
+	@Column(nullable = false)
+	private Integer quantidade;
+	
+	@Column(nullable = false)
+	private BigDecimal precoUnitario;
+	
+	@Column(nullable = false)
+	private BigDecimal precoTotal;
+	
+	@Column(nullable = true)
+	private String Observacao;
 	
 	@ManyToOne
-	@JoinColumn(nullable = false,name = "estadoId")
-	private Estado estado;
+	@JoinColumn(name = "pedido_Id", nullable = false)
+	private Pedido pedido;
+	
+	@ManyToOne
+	@JoinColumn(name = "produto_id", nullable = false)
+	private Produto produto;
+	
+	
 
 }
---
+++ src/main/resources/db/migration/V00007__Renomeia coluna permisssao_id para permissao_id na tabeala grupo_permissao.sql
@@ -0,0 +1,2 @@
+-- V00007__Renomeia coluna permisssao_id para permissao_id na tabeala grupo_permissao.sql
+ALTER TABLE GRUPO_PERMISSAO RENAME COLUMN PERMISSSAO_ID TO PERMISSAO_ID;
